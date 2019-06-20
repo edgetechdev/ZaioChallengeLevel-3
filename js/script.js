6a43d6fa97bd86109675dc56ec4e06fd6efc8672
edgetechdev <edgetech.io.dev@gmail.com>
2019-06-20 13:29:00 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
+const userName = document.getElementById("userName");
+const surName = document.getElementById("surName");
+const age = document.getElementById("age");
+//const createPassword = document.getElementById("createPassword");
+//const confirmPassword = document.getElementById("confirmPassword");
+const password = document.getElementById("Password");
 
 signUpButton.addEventListener("click", () =>
   container.classList.add("right-panel-active")
@@ -9,3 +15,19 @@ signUpButton.addEventListener("click", () =>
 signInButton.addEventListener("click", () =>
   container.classList.remove("right-panel-active")
 );
+
+function check_info() {
+  var cp = document.getElementById("createPassword").value;
+  var con = document.getElementById("confirmPassword").value;
+
+  if (cp == "" || con == "") {
+    alert("check fields");
+    return false;
+  }
+  else if(cp != con){
+    alert("no match");
+  }
+  else{
+    alert("matches");
+  }
+}
